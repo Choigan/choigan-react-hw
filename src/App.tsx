7472da427d2d8f9c844caa7915bdf46df16e5277
Choigan <chojgan.ondar95@gmail.com>
2023-12-10 21:38:07 +0700

    init: hw-3

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./App.css";
+import { type } from "os";
+
+type userNameDataProps = {
+  userName: string;
+};
+
+const userData = {
+  userName: "Джон Уик",
+};
+
+const UserNameWrite = (props: userNameDataProps) => {
+  return <div>{props.userName}</div>;
+};
 
 function App() {
-  return <div className="App">ываыаоыро</div>;
+  return (
+    <div>
+      <UserNameWrite userName={userData.userName} />
+    </div>
+  );
 }
 
 export default App;
